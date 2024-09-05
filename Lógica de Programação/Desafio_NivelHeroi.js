9b36e8f5eb3e3f57e67a211f10e916155aa7bc01
Isabela Bergamo <105016708+itBergamo@users.noreply.github.com>
2024-09-05 00:09:53 -0300

    chore: Desafio do Herói - desafio LP1 - DIO finalizado

---
+++ Lógica de Programação/Desafio_NivelHeroi.js
@@ -0,0 +1,14 @@
+let nome = "Isabela Bergamo";
+let XP = 10050;
+let nivel;
+
+if (XP <= 1000) nivel = "Ferro";
+else if (XP <= 2000) nivel = "Bronze";
+else if (XP <= 5000) nivel = "Prata";
+else if (XP <= 7000) nivel = "Ouro";
+else if (XP <= 8000) nivel = "Platina";
+else if (XP <= 9000) nivel = "Ascendente";
+else if (XP <= 10000) nivel = "Imortal";
+else nivel = "Radiante";
+
+return console.log("O Herói de nome " + nome + " está no nível de " + nivel);
